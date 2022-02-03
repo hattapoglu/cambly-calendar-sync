e5ee4283ac5a5b8cc36ad22f53925de7cf1e0ab2
Omer Hattapoglu <omerhattapoglu1@gmail.com>
2022-02-03 16:12:22 +0300

    Refactored js

--- popup.js
+++ popup.js
@@ -1,78 +1,31 @@
-const form = document.getElementById('control-row')
-const reservationList = document.getElementById('reservations')
+const div = document.getElementById('reservation')
+const reservationList = document.getElementById('list')
 const message = document.getElementById('message')
-const downloadForm = document.createElement('form')
+const getButton = document.getElementById('get')
+const downloadButton = document.getElementById('download')
+
 const hostUrl = 'www.cambly.com'
 const reservationUrl = 'https://www.cambly.com/api/reservations?language=en&cancelled=false&scrub=true'
 const scheduleUrl = 'https://www.cambly.com/en/student/schedule/'
+
+const fetchOptions = {
+  headers: {
+    cookie: {},
+  },
+}
+
 const defaults = {
   title: 'Cambly Lesson',
-  url: 'https://www.cambly.com',
+  url: hostUrl,
 }
 
-form.addEventListener('submit', handleFormSubmit)
-
-async function handleFormSubmit(event) {
-  event.preventDefault()
-
-  clearMessage()
-
-  try {
-    let session = await getDomainCookies(hostUrl)
-    let sessionId = session.filter((el) => el.name === 'session')[0].value
-    chrome.storage.sync.get(['student_id'], async function (storage) {
-      const url = reservationUrl + `&studentId=${storage.student_id}&start=${new Date().getTime()}`
-      const opts = {
-        headers: {
-          cookie: `session=${sessionId}`,
-        },
-      }
-
-      const res = await fetch(url, opts)
-      const { result } = await res.json()
-      const transformed = transformCalendarData(result)
-      const file = createICSFromEvents(transformed)
-      showReservations(transformed)
-      createDownload(file)
-    })
-  } catch (error) {
-    setMessage(error)
-  }
+const errors = {
+  authentication: 'Please log in to Cambly and try again.',
+  storage: 'Student id could not be found.',
+  session: `Please go to ${hostUrl}, log in and click the button again.`,
 }
 
-async function getDomainCookies(domain) {
-  let cookies
-  try {
-    cookies = await chrome.cookies.getAll({ domain })
-
-    if (cookies.length === 0) {
-      return 'No cookies found'
-    }
-  } catch (error) {
-    return `Unexpected error: ${error.message}`
-  }
-
-  return JSON.parse(JSON.stringify(cookies))
-}
-
-function showReservations(results) {
-  results.map((res) => {
-    reservationList.innerHTML += `<li>Start time: ${epochToReadable(res.start)}, Duration: ${res.duration} </li>`
-  })
-}
-
-function createDownload(file) {
-  let datauri = `data:text/plain;charset=utf-8,${file}`
-  var element = document.createElement('a')
-  element.setAttribute('href', datauri)
-  element.setAttribute('download', 'cambly_calendar.ics')
-  element.innerText = 'Download Reservations!'
-  document.body.appendChild(element)
-}
-
-function epochToReadable(epochTime) {
-  return new Date(epochTime).toLocaleString()
-}
+getButton.addEventListener('click', getReservations)
 
 function setMessage(str) {
   message.textContent = str
@@ -84,16 +37,80 @@ function clearMessage() {
   message.textContent = ''
 }
 
-function transformCalendarData(arr) {
-  const transformed = []
-  arr.forEach((el, index) => {
-    const obj = { start: el.startTime.$date }
-    if (el.endTime.$date) obj.end = el.endTime.$date
-    if (el.minutes) obj.duration = el.minutes
-    if (el._id.$oid) obj.url = scheduleUrl + el._id.$oid
-    transformed[index] = obj
+async function getReservations() {
+  clearMessage()
+  document.body.classList.remove('active')
+
+  // const cookies = await getCookies(hostUrl)
+  // const session = cookies.filter((el) => el.name === 'session')
+  // if (!session.length) {
+  //   setMessage(errors.authentication)
+  //   throw new Error(errors.authentication)
+  // }
+  // console.log('is that even come to here???!')
+  // const sessionId = session[0].value
+  // fetchOptions.headers.cookie
+  // fetchOptions.headers.cookie = `session=${sessionId}`
+  const studentId = await getKeyFromStorage('student_id')
+  fetch(`${reservationUrl}&studentId=${studentId}&start=${new Date().getTime()}`)
+    .then(async (res) => {
+      console.log(res, res.ok, res.status)
+      const { result } = await res.json()
+      const results = result.map((res) => transformCalendarData(res))
+      setReservations(results)
+      setDownload(results)
+    })
+    .catch((error) => {
+      console.log(error.code, error.message)
+    })
+}
+
+// async function getCookies(domain) {
+//   try {
+//     const cookies = await chrome.cookies.getAll({ domain })
+//     if (!cookies.length) {
+//       setMessage(errors.session)
+//       throw new Error(errors.session)
+//     }
+//     return cookies
+//   } catch (error) {
+//     console.log(error.message)
+//   }
+// }
+
+async function getKeyFromStorage(key) {
+  try {
+    let storage = await chrome.storage.sync.get([key])
+    if (!storage[key]) {
+      setMessage(errors.storage)
+      throw new Error(errors.storage)
+    }
+    return storage[key]
+  } catch (error) {
+    console.log(error.message)
+  }
+}
+
+function transformCalendarData(result) {
+  return { start: result.startTime.$date, duration: result.minutes, id: result._id.$oid }
+}
+
+function setReservations(results) {
+  reservationList.innerHTML = ''
+  results.map((res) => {
+    reservationList.innerHTML += `<li>${epochToReadable(res.start)}, Duration: ${res.duration} Minutes</li>`
   })
-  return transformed
+  document.body.classList.add('active')
+}
+
+function setDownload(results) {
+  let file = createICS(results)
+  let datauri = `data:text/plain;charset=utf-8,${file}`
+  downloadButton.setAttribute('href', datauri)
+}
+
+function epochToReadable(epochTime) {
+  return new Date(epochTime).toLocaleString().replace(/\//g, '.').replace(',', ' @')
 }
 
 function formatDate(timestamp) {
@@ -104,7 +121,7 @@ function formatDuration(minutes) {
   return `PT${minutes}M`
 }
 
-function createICSFromEvents(events) {
+function createICS(events) {
   let icsFormat = ''
   icsFormat += 'BEGIN:VCALENDAR\r\n'
   icsFormat += 'VERSION:2.0\r\n'
@@ -121,7 +138,6 @@ function formatEvent(ev) {
   let title = event.title,
     start = event.start,
     duration = event.duration,
-    end = event.end,
     url = event.url
 
   let icsFormat = ''
@@ -131,8 +147,6 @@ function formatEvent(ev) {
 
   if (duration) {
     icsFormat += `DURATION:${formatDuration(duration)}\r\n`
-  } else if (end) {
-    icsFormat += `DTEND:${formatDate(end)}\r\n`
   }
 
   if (url) {
